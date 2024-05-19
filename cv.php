4f233b9e37c5cfb41171deddd41674cc85f4bdc8
methila007 <methilamomena@gmail.com>
2024-05-19 21:09:28 +0600

    Add files via upload

---
+++ cv.php
@@ -0,0 +1,219 @@
+[11:01 am, 17/05/2024] Momena: <!DOCTYPE html>
+<html>
+<head>
+<title>2020-2-60-037</title>
+</head>
+<body>
+<table width="1300">
+<tr>
+<td><h2> Momena Khatun Methila </h2></td>
+<td align ="right"; rowspan="4"><img src = "images/unnamed.jpg" height="180"width="90"/></td>
+</tr>
+<tr>
+<td>Dhaka,Bangladesh</td>
+</tr>
+<tr>
+<td>Email: methilaraiyan@gmail.com</td>
+</tr>
+<tr>
+<td>Tel: 01628210561</td>
+</tr>
+</table>
+<br>
+<h3> Career Objective</h3><hr/><br/>
+<p>Focused and diligent graduate in data sciences looks to leverage in-depth knowledge of data analysis to drive success in the business intelligence team at "XYZ". Participated in research projects while at university. Proficient in Python, and Excel.</p><br/>
+<h3> Present Status</h3><hr/><br/>
+<p>Undergraduate student at East West University</p><br/>
+<h3> Education</h3><hr/><br/>
+<table width="1300"; border="">
+<tr>
+<td><h5> Exam</h5></td>
+<td><h5> Board/University</h5></td>
+<td><h5> Year</h5></td>
+<td><h5> Name of Institution</h5></td>
+<td><h5> GPA </h5></td>
+</tr>
+<tr>
+<td> SSC </td>
+<td> Dhaka </td>
+<td> 2017 </td>
+<td> MMSC </td>
+<td> 5.00 </td>
+</tr>
+</tr>
+<tr>
+<td> HSC </td>
+<td> Dhaka </td>
+<td> 2019 </td>
+<td> MMSC </td>
+<td> 4.25 </td>
+</tr>
+</tr>
+</table>
+<br>
+<h3> Computer Skills</h3><hr/><br/>
+<table>
+<tr>
+<td> Programming Languages</td>
+<td>: </td>
+<td>C, C++, Java, Php, Python </td>
+</tr>
+<tr>
+<td> Web based skills</td>
+<td>: </td>
+<td>HTML, CSS, PHP </td>
+</tr>
+<tr>
+<td> Database skills</td>
+<td>: </td>
+<td>SQL </td>
+</tr>
+<tr>
+<td>Scripting skills</td>
+<td>: </td>
+<td> </td>
+</tr>
+<tr>
+<td>Development Tools </td>
+<td>: </td>
+<td>VS code,Git-hub </td>
+</tr>
+<tr>
+<td> Others</td>
+<td>: </td>
+<td> </td>
+</tr>
+</table>
+<h3> Professional Experience</h3><hr/><br/>
+<p>No experience</p>
+<h3> Projects</h3><hr/><br/>
+<p>Do not work in any professional project.</p>
+<h3>Academic Projects</h3><hr/><br/>
+<ul>
+<li>TIC TAC TOE
+<ul>
+<li>Generally the game board size is 3x3 but I developed the game where users can input the board size in n*n form.</li>
+</ul>
+</li>
+</ul>
+<ul>
+    <li>PORTAL MANAGEMENT SYSTEM
+    <ul>
+    <li>I with my team developed a system that would help a portal admin to manage his system, student information, and subject information.</li>
+    </ul>
+    </li>
+    </ul>
+    <ul>
+        <li>DENTAL CLINIC PROBLEM
+        <ul>
+        <li>  We had to synchronize the activities of a dental clinic using threads, semaphores and MUTEX locks.</li>
+        </ul>
+        </li>
+        </ul>
+        <ul>
+            <li>N QUEEN
+            <ul>
+            <li>Generally the game board size is n x n but I developed the game where users can input the board size in n*n form.</li>
+            </ul>
+            </li>
+            </ul>
+            <ul>
+                <li>SHOP MANAGEMENT SYSTEM
+                <ul>
+                <li>My team developed a system to manage price, stock, quantity, customers and print payment slip.</li>
+                </ul>
+                </li>
+                </ul>
+                <ul>
+<h3> Achievments</h3><hr/><br/>
+<ul>
+<li> No achievments
+</li>
+</ul>
+<h3> Language</h3><hr/><br/>
+<ul>
+    <li> Bangla
+    </li>
+    </ul>
+    <ul>
+        <li> English
+        </li>
+        </ul>
+<h3> Personal Information</h3><hr/><br/>
+<table align="center">
+<tr>
+<td>Full Name </td>
+<td>: </td>
+<td> Momena Khatun Methila </td>
+</tr>
+<tr>
+<td> Father's Name</td>
+<td>: </td>
+<td> Manik Sarder </td>
+</tr>
+<tr>
+<td>Mother's Name </td>
+<td>: </td>
+<td> Jharna </td>
+</tr>
+<tr>
+<td>Date of Birth</td>
+<td>: </td>
+<td> 22/12/2001 </td>
+</tr>
+
+        <tr>
+            <td>Sex</td>
+            <td>: </td>
+            <td> Female </td>
+            </tr>
+            <tr>
+                <td>Marital Status</td>
+                <td>: </td>
+                <td>Single </td>
+                </tr>
+                <tr>
+                    <td>Nationality</td>
+                    <td>: </td>
+                    <td> Bangladesh</td>
+                    </tr>
+                    <tr>
+                        <td>Religion</td>
+                        <td>: </td>
+                        <td>  	Islam</td>
+                        </tr>
+                        <tr>
+                            <td>Blood Group</td>
+                            <td>: </td>
+                            <td> B+</td>
+                            </tr>
+                            <tr>
+                                <td>Present Address</td>
+                                <td>: </td>
+                                <td> 61/1, Dholpur, Dhaka-1204 </td>
+                                </tr>
+                                <tr>
+                                    <td>Permanent Address</td>
+                                    <td>: </td>
+                                    <td> 61/1, Dholpur, Dhaka-1204 </td>
+                                    </tr>
+</table>
+<br>
+<h3> References</h3><hr/><br/>
+<h5>Dr. Salah Uddin </h5><br/>
+<p> 
+
+    Professor, Department of Computer Science and Engineering<br/>
+    
+    East West University, Dhaka<br/>
+    
+    Email: dsu@ewubd.edu</p><br/>
+<h5> Saddam Hossain Khan</h5><br/>
+<p> Department of Computer Science and Engineering<br/>
+    
+    East West University<br/>
+    
+    Email: saddam.cse@ewubd.edu </p><br/>
+<p align = "center">Copyright ©️Momena Khatun Methila, 2024</p>
+</body>
+</html>
